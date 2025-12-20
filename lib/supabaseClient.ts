65c531acd97e0149dd7468e1a2eeeeceef0e22d5
kenan <k.karimli@ufaz.az>
2025-12-20 04:06:19 +0400

    Update Supabase client configuration

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,6 @@
+import { createClient } from "@supabase/supabase-js"
 
-import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-const supabaseUrl = process.env.local.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.local.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey)
